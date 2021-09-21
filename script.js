55078ccfd9778ccba9bb18faadaa490f5f474c57
Kathy Coffee <coffeekl8807@gmail.com>
2021-09-20 21:01:50 -0400

    First JavaScript exercise completed with bonuses

---
+++ script.js
@@ -0,0 +1,67 @@
+// 1
+console.log(3);
+
+// 2
+// The five main data types (AKA Primitives) are:
+/* 
+Numbers
+Stings
+Booleans
+Null
+Undefined
+*/
+
+// 3
+// The six operator symbols are:
+/*
+"+" plus sign for addition
+"-" minus sign for subtraction
+"*" for multiplication
+"/" for division
+"**" Exponentiation
+"%" Modulus
+ */
+
+// 4
+console.log(6%5);
+console.log(1%5);
+
+// 5
+console.log("I\'m Mary Poppins, y\'all!");
+
+// 6
+console.log("Hawk"+"eye");
+
+// 7
+console.log("Kathy Coffee".length);
+
+// 8
+console.log("Avengers: Endgame".length);
+console.log("Avengers: Endgame"[16]);
+
+// Bonus
+// 1
+console.log("\\");
+
+// 2
+console.log((5**4)-9);
+
+// 3
+console.log("I Am Iron Man".length - 13);
+
+// 4
+console.log("CAP"[1]);
+console.log("Vision"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+let m = 25; 
+let c = 4; 
+let u = m + c;
+
+
